89cee2456de6b6c1b2e4bda275d26add02eaed67
JGEC1991 <89346164+JGEC1991@users.noreply.github.com>
2025-04-07 10:12:49 -0600

    Initial commit from your app

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,15 @@
         "./src/**/*.{js,ts,jsx,tsx}",
       ],
       theme: {
-        extend: {},
+        extend: {
+          screens: {
+            'sm': '640px',
+            'md': '768px',
+            'lg': '1024px',
+            'xl': '1280px',
+            '2xl': '1536px',
+          },
+        },
       },
-      plugins: [],
+      plugins: [require('@tailwindcss/typography'), require('daisyui')],
     }
